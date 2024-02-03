5937a1ff8a89536f81ec995fffc4fad6c7cc674f
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-03 18:22:37 -0300

    Finish Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,56 @@
-let data = [
-    {
-        id: 1,
-        title: 'estudar html'
-    }, {
-        id: 2,
-        title: 'estudar css'
-    }, {
-        id: 3,
-        title: 'estudar javascript'
-    }, {
-        id: 4,
-        title: 'estudar php'
-    }
-]
+let data = []
 
-data.forEach(task => {
-    let li = document.createElement('li')
-    li.innerHTML = `
-        <input type="checkbox" id="task-${task.id}">
-        <label for="task-${task.id}">${task.title}</label>
-    `
-    li.querySelector('input').addEventListener('change', e => {
-        if (e.target.checked) {
-            li.classList.add('complete')
-        } else {
-            li.classList.remove('complete')
-        }
+function renderTodo()
+{
+    
+    document.querySelector('.todo').innerHTML = ''
+
+    data.forEach(task => {
+        let li = document.createElement('li')
+        li.innerHTML = `
+            <input type="checkbox" id="task-${task.id}">
+            <label for="task-${task.id}">${task.title}</label>
+            <button type"button">X</button>
+        `
+
+        li.querySelector('input').addEventListener('change', e => {
+            if (e.target.checked) {
+                li.classList.add('complete')
+            } else {
+                li.classList.remove('complete')
+            }
+        })
+
+        li.querySelector('button').addEventListener('click', e => {
+            let button = e.target
+            let li = button.parentNode
+            let input = li.querySelector('input')
+            let id = input.id
+            let idArray = id.split('-')
+            let todoId = idArray[1]
+            let title = li.querySelector('label').innerText
+
+            if (confirm(`Você deseja realmente excliur a tarefa ${title}?`)) {
+                data = data.filter(task => task.id !== parseInt(todoId))
+                renderTodo()
+            }
+        })
+
+        document.querySelector('.todo').append(li)
     })
-    document.querySelector('.todo').append(li)
-})
+    
+}
 
 document.querySelector('#new-task').addEventListener('keyup', e => {
     if (e.key === 'Enter') {
-        console.log(e.target.value)
+        data.push({
+            id: data.length + 1,
+            title: e.target.value
+        })
+
+        e.target.value = ''
+        renderTodo()
     }
 })
+
+renderTodo()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# To Do
+
+## Lista de Tarefas
+
+Projeto feito com **HTML5**, **CSS3** e **JavaScript**
